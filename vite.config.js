8e0e0c0c03fcdb939f1deb084f2036daa5715b95
Sølve Monteiro <solve.monteiro@conta.no>
2025-09-30 09:10:14 +0200

    wipall

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './',
+  server: {
+    open: true,
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
