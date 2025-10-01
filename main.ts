a753d0e50146a9d5075cf6fcc20a410f2575d90b
Sølve Monteiro <solve.monteiro@conta.no>
2025-10-01 09:04:36 +0200

    ## - Extract styles, clean up imports

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import Reveal from 'reveal.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes.js';
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
-import 'reveal.js/dist/reset.css';
-
+import './style.css'
 
 Reveal.initialize({
     hash: true,
